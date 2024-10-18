efa7b702720176d4ccd9186a8759e78051a7530d
Jonh Carvalho <jonh.carvalho@professores.ibmec.edu.br>
2024-10-18 17:32:44 -0300

    update

--- docs/_Disciplina/Code/DemoDjango8002/app/admin.py
+++ docs/_Disciplina/Code/DemoDjango8002/app/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-# Register your models here.
-from app.models import Aluno
+from app.models import Aluno, Employee, Department
 
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'role', 'department')
+    search_fields = ('name','role',)
+    list_filter = ('department',) #filter
+    ordering = ('name',)
+    
 admin.site.register(Aluno)
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Department)
--- docs/_Disciplina/Code/DemoDjango8002/app/models.py
+++ docs/_Disciplina/Code/DemoDjango8002/app/models.py
@@ -10,3 +10,17 @@ class Aluno(models.Model):
 
     def __str__(self):
         return f"{self.nome}"
+    
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
